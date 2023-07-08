1a22bd729d64c1b3988c7b075b3c3641dd9b2a5f
Pau M <paum@Paus-MacBook-Pro.local>
2023-07-08 00:11:52 -0400

    swiper tutorial

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-new Swiper('#swiper-1', {
-    effect: 'fade',
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    }
-});
+// new Swiper('#swiper-1', {
+//     effect: 'fade',
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction:false
+//     },
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true
+//     },
+//     lazyLoading: true,
+//     loop: true
+// });
+
+// new Swiper('#swiper-2', {
+//     slidesPerView: 1.5,
+//     centeredSlides: true,
+//     spaceBetween: 12,
+//     lazyLoading: true,
+//     loop: true,
+//     keyboard: {
+//         enabled: true
+//     },
+//     navigation: {
+//         nextEl: '#nav-right',
+//         prevEl: '#nav-left',
+//     },
+//     pagination: {
+//         el:('#swiper-2 .swiper-custom-pagination'),
+//         clickable: true,
+//         renderBullet: function (index, className) {
+//             return `<div class=${className}><span class='number'>${index + 1}</span>
+//             <span class="line"></span></div>`
+//         }
+//     }
+// })
 
 // new Swiper('#swiper-1', {
 //     slidesPerView: 3,
@@ -29,4 +58,4 @@ new Swiper('#swiper-1', {
 //     scrollbar: {
 //       el: '.swiper-scrollbar',
 //     },
-//   });
+//   });http://127.0.0.1:5500/Assets/sunset_dream_poster.svg
